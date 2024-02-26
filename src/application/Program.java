bd86ea279bfdc47f3d326fcd295882e424c37bb5
André Prates <engandreprates@gmail.com>
2024-02-26 20:55:55 -0300

    Fourth way

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,9 +20,7 @@ public class Program {
 		
 		// Implementation anonymous function lambda expression
 		// arrow function
-		Comparator<Product> comp = (p1, p2) -> {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());			
-		};
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());			
 		
 		list.sort(comp);
 		
